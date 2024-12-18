0b30f578af882b25c43fd176f17f02d22aa33177
NkwaTambe <nkwajudetambe@gmail.com>
2024-12-18 17:00:24 +0100

    update url

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"; // Tailwind CSS
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+
+  <BrowserRouter basename="/">
+     <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </BrowserRouter>
 );
