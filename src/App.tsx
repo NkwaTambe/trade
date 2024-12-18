e95f824518753d050b6d8d8803395247e6d8f466
NkwaTambe <nkwajudetambe@gmail.com>
2024-12-18 17:03:06 +0100

    standards

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,5 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { Home } from "./components/Home";
-// import { Navbar, Footer } from "./components/Shared";
-// import GetStarted from "./pages/GetStarted";
-// import About from "./pages/About";
-// import Testimonials from "./pages/Testimonials";
-// import Contact from "./pages/Contact";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
-
-// const App: React.FC = () => {
-//   return (
-//     <Router>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/courses" element={<div>Courses Page</div>} />
-//         <Route path="/contact" element={<Contact />} />
-//         <Route path="/get-started" element={<GetStarted />} />
-//         <Route path="/testimonials" element={<Testimonials />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/signup" element={<Signup />} />
-//       </Routes>
-//       <Footer />
-//     </Router>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Navbar, Footer } from "./components/Shared";
 import GetStarted from "./pages/GetStarted";
@@ -61,4 +29,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"; // Tailwind CSS
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-
-  <BrowserRouter basename="/">
-     <React.StrictMode>
+  <React.StrictMode>
     <App />
   </React.StrictMode>,
-  </BrowserRouter>
 );
